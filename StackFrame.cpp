41051a13f45664ca687328eb4bccf2fafcb925ef
minh <tangvanminh1@gmail.com>
2022-04-28 18:02:13 +0700

    first change

--- StackFrame.cpp
+++ StackFrame.cpp
@@ -22,59 +22,59 @@ enum error_type {
 };
 
 
-class localVariableSpace {
-public:
-    class Node;
-protected:
-    Node* root;
-    int count;
-public:
-    localVariableSpace() {
-        this->root = NULL;
-        count = 0;
-    }
-    //more func
-
-
-
-
-public:
-    //class Node
-    class Node {
-    private:
-        string key;
-        float value;
-        int type;
-        Node* left, * right;
-        balance_factor balance;
-        friend class localVariableSpace;
-    public:
-        Node() {
-            this->left = this->right = NULL;
-        }
-        Node(string key, float value, int type, Node* left, Node* right) {
-            this->key = key;
-            this->value = value;
-            this->type = type;
-            this->left = left;
-            this->right = right;
-            this->balance = EH;
-        }
-        Node(string key, float value, int type) {
-            this->key = key;
-            this->value = value;
-            this->type = type;
-            this->left = this->right = NULL;
-            this->balance = EH;
-        }
-        ~Node() {
-            this->key = "";
-            this->value = 0;
-            this->type = 0;
-            this->left = this->right = NULL;
-        }
-    };
-};
+//class localVariableSpace {
+//public:
+//    class Node;
+//protected:
+//    Node* root;
+//    int count;
+//public:
+//    localVariableSpace() {
+//        this->root = NULL;
+//        count = 0;
+//    }
+//    //more func
+//
+//
+//
+//
+//public:
+//    //class Node
+//    class Node {
+//    private:
+//        string key;
+//        float value;
+//        int type;
+//        Node* left, * right;
+//        balance_factor balance;
+//        friend class localVariableSpace;
+//    public:
+//        Node() {
+//            this->left = this->right = NULL;
+//        }
+//        Node(string key, float value, int type, Node* left, Node* right) {
+//            this->key = key;
+//            this->value = value;
+//            this->type = type;
+//            this->left = left;
+//            this->right = right;
+//            this->balance = EH;
+//        }
+//        Node(string key, float value, int type) {
+//            this->key = key;
+//            this->value = value;
+//            this->type = type;
+//            this->left = this->right = NULL;
+//            this->balance = EH;
+//        }
+//        ~Node() {
+//            this->key = "";
+//            this->value = 0;
+//            this->type = 0;
+//            this->left = this->right = NULL;
+//        }
+//    };
+//};
 
 StackFrame::StackFrame() : opStackMaxSize(OPERAND_STACK_MAX_SIZE), localVarSpaceSize(LOCAL_VARIABLE_SPACE_SIZE) {}
 
